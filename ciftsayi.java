2e9cb039e9061fcb14981223be52fa52dcd95420
hatice demirtas <98237665+hdmirts@users.noreply.github.com>
2022-05-24 00:36:50 +0300

    Create ciftsayi.java

---
+++ ciftsayi.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class ciftsayi {
+    public static void main(String[] args) {
+        int d; 
+        int toplam = 0, count =0;
+    
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Sayıyı Giriniz :");
+        d = inp.nextInt();
+
+        for(int a =1; a <= d; a++){
+            if (a%12==0){
+                toplam += a;
+                count++;                               
+            }
+
+        }
+        System.out.println("3 ve 4'e tam bölünen sayıların ortalaması :"  + (toplam/count));
+    }
+}
